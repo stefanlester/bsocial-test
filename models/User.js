a9cc749430b59ecbc9a1d1ffc7b5b06d55122b58
stefanlester <stefanamoah001@gmail.com>
2021-12-31 15:38:12 +0100

    refactored user model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,28 @@
+const mongoose = require('mongoose');
 const { model, Schema } = require('mongoose');
+const validator = require('validator');
 
-const userSchema = new Schema({
-  username: String,
-  password: String,
-  email: String
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Please provide email'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide valid email',
+    }
+}
 });
 
 module.exports = model('User', userSchema);
